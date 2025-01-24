89e77a17d70b567af4faee5402697619208f7c40
Ilya Drugov <thehardkor@yandex.ru>
2025-01-24 21:17:29 +0400

    version 1.2 feat: search/media/settingsActivity (Light Mode)

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.playlistmaker
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        val backButton = findViewById<ImageView>(R.id.toolBar)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+}
