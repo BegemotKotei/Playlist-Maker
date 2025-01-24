a240eac9cc196774c375b162a104325692cafeed
Ilya Drugov <thehardkor@yandex.ru>
2025-01-24 21:27:24 +0400

    version 1.2 fix: добавил возврат из mediaLib и search

--- app/src/main/java/com/example/playlistmaker/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MediaLibraryActivity.kt
@@ -1,15 +1,20 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MediaLibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_media_library)
+
+        val backButton = findViewById<ImageView>(R.id.toolBar)
+
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,15 +1,19 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_serarch)
+        val backButton = findViewById<ImageView>(R.id.toolBar)
+
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
