a4774a4e7742700f6d1873e69571fc5c25c42e5f
Ilya Drugov <thehardkor@yandex.ru>
2025-01-31 12:26:12 +0400

    version 1.2 fix

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -3,16 +3,25 @@ package com.example.playlistmaker
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class SettingsActivity : AppCompatActivity() {
-
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<ImageView>(R.id.arrow_back_settings)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tool_bar_settings)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         backButton.setOnClickListener {
             finish()
