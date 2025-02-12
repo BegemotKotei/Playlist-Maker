f9944595972a0c8c68af52829b46d6a23f6edf93
Ilya Drugov <thehardkor@yandex.ru>
2025-02-12 21:14:35 +0400

    version 2.2 feat: изменил тему под цвет, добавил поиск

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,20 +1,39 @@
 package com.example.playlistmaker
 
+import android.app.Activity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+    private var searchString = ""
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH, searchString)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_serarch)
+        setContentView(R.layout.activity_search)
 
         val toolbar = findViewById<Toolbar>(R.id.tool_bar_search)
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowTitleEnabled(true)
+
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val searchField = findViewById<EditText>(R.id.searchText)
+
+        searchField.setText(searchString)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tool_bar_search)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -22,6 +41,44 @@ class SearchActivity : AppCompatActivity() {
             insets
         }
 
+
+        val simpleTextWatcher = object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+                searchString = searchField.toString()
+            }
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+
+        }
+
+        searchField.addTextChangedListener(simpleTextWatcher)
+
+        clearButton.setOnClickListener {
+            searchField.setText("")
+
+            val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(searchField.windowToken,0)
+        }
+
+
         toolbar.setNavigationOnClickListener { finish() }
+    }
+
+   private fun clearButtonVisibility(s:CharSequence?):Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    companion object{
+        const val SEARCH = "SEARCH"
     }
 }
